89e701c4842de91877a3e9e361cb10c9e6e8bf6a
Dustin <dustypants2005@hotmail.com>
2019-01-09 20:00:12 -0500

    fix projectile error and adjust player
    Some projectiles needed the onCollisionEnter as some are not triggers. Adjusted player inputs as the defaults were a bit slow

--- Assets/_scripts/Charaters/Projectile.cs
+++ Assets/_scripts/Charaters/Projectile.cs
@@ -46,6 +46,52 @@ namespace dustypants {
       }
     }
 
+    void OnCollisionEnter(Collision hit) {
+      if (!hasCollided) {
+        hasCollided = true;
+        //transform.DetachChildren();
+        impactParticle = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, impactNormal)) as GameObject;
+        //Debug.DrawRay(hit.contacts[0].point, hit.contacts[0].normal * 1, Color.yellow);
+
+        var audsrc = impactParticle.GetComponent<AudioSource>();
+        if(audsrc != null && LowerVolume){
+          audsrc.volume = Volume;
+        }
+        if (hit.gameObject.CompareTag("Destructible")){// Projectile will destroy objects tagged as Destructible
+          Destroy(hit.gameObject);
+        }
+
+        var enemy = hit.gameObject.GetComponent<Damagable>();
+        if(enemy != null && enemy.CompareTag(TagToDamage)){
+          enemy.AdjustHealth(-Damage);
+        }
+
+        FireDoorCheck(hit.gameObject.GetComponentInParent<IFireDoor>());
+        FrostDoorCheck(hit.gameObject.GetComponentInParent<IFrostDoor>());
+
+        //yield WaitForSeconds (0.05);
+        foreach (GameObject trail in trailParticles){
+          GameObject curTrail = transform.Find(projectileParticle.name + "/" + trail.name).gameObject;
+          curTrail.transform.parent = null;
+          Destroy(curTrail, 3f);
+        }
+        Destroy(projectileParticle, 3f);
+        Destroy(impactParticle, 5f);
+        Destroy(gameObject);
+        //projectileParticle.Stop();
+        ParticleSystem[] trails = GetComponentsInChildren<ParticleSystem>();
+        //Component at [0] is that of the parent i.e. this object (if there is any)
+        for (int i = 1; i < trails.Length; i++) {
+          ParticleSystem trail = trails[i];
+          if (!trail.gameObject.name.Contains("Trail"))
+            continue;
+
+          trail.transform.SetParent(null);
+          Destroy(trail.gameObject, 2);
+        }
+      }
+    }
+
     void OnTriggerEnter(Collider other) {
       if(other.isTrigger){ // should do nothing if we hit a trigger
         return;
