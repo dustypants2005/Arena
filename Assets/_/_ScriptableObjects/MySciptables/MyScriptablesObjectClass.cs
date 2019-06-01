b26a0077757af6aabf2bcb575bae78527ac21767
dustin <dustypants2005@hotmail.com>
2019-06-01 09:45:40 -0400

    ignore assets and meta, namespace removed for scriptable object

--- Assets/_/_ScriptableObjects/MySciptables/MyScriptablesObjectClass.cs
+++ Assets/_/_ScriptableObjects/MySciptables/MyScriptablesObjectClass.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace dustypants {
   [CreateAssetMenu(fileName = "Data", menuName = "Scritabels/PlayerInfo", order = 1)]
   public class MyScriptablesObjectClass : ScriptableObject {
     public string Name = "Player";
   }
-}
